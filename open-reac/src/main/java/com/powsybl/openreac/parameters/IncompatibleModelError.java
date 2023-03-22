efa4d4e247c7867d7b8350273fd1db81b32d7c6f
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-03-22 15:00:42 +0100

    Missing licence + author
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

--- open-reac/src/main/java/com/powsybl/openreac/parameters/IncompatibleModelError.java
+++ open-reac/src/main/java/com/powsybl/openreac/parameters/IncompatibleModelError.java
@@ -1,8 +1,15 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openreac.parameters;
 
 /**
  * Throw this error when the interface between ampl and java is not correct.
  * This is an internal OpenReac error. It is not the user fault.
+ * @author Nicolas Pierre <nicolas.pierre at artelys.com>
  */
 public class IncompatibleModelError extends Error {
     public IncompatibleModelError(String message){
--- open-reac/src/main/java/com/powsybl/openreac/parameters/InvalidParametersException.java
+++ open-reac/src/main/java/com/powsybl/openreac/parameters/InvalidParametersException.java
@@ -1,7 +1,16 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.openreac.parameters;
 
 import com.powsybl.commons.PowsyblException;
 
+/**
+ * @author Nicolas Pierre <nicolas.pierre at artelys.com>
+ */
 public class InvalidParametersException extends PowsyblException {
     public InvalidParametersException(String message){
         super(message);
