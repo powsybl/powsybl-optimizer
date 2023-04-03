79fff23deca9ddd50aef69feaf56a4e1037f4e4b
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-04-03 11:06:40 +0200

    rename OpenReac objective class + added parameter specific to BETWEEN_HIGH_AND_LOW_VOLTAGE_PROFILE
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

--- open-reac/src/main/java/com/powsybl/openreac/parameters/input/algo/OpenReacOptimisationObjective.java
+++ open-reac/src/main/java/com/powsybl/openreac/parameters/input/algo/OpenReacOptimisationObjective.java
@@ -9,7 +9,7 @@ package com.powsybl.openreac.parameters.input.algo;
 /**
  * @author Nicolas Pierre <nicolas.pierre at artelys.com>
  */
-public enum OpenReacObjective implements OpenReacAlgoParam {
+public enum OpenReacOptimisationObjective implements OpenReacAlgoParam {
 
     MIN_GENERATION(0), BETWEEN_HIGH_AND_LOW_VOLTAGE_PROFILE(1), SPECIFIC_VOLTAGE_PROFILE(2);
 
@@ -20,7 +20,7 @@ public enum OpenReacObjective implements OpenReacAlgoParam {
     /**
      * @param amplKey value used in param_algo.txt to define the given objective.
      */
-    OpenReacObjective(int amplKey) {
+    OpenReacOptimisationObjective(int amplKey) {
         this.name = OBJECTIVE_PARMA_KEY;
         this.amplKey = amplKey;
     }
---
+++ open-reac/src/main/java/com/powsybl/openreac/parameters/input/algo/OptimisationVoltageRatio.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2023, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.powsybl.openreac.parameters.input.algo;
+
+/**
+ * Must be used with {@link OpenReacOptimisationObjective#BETWEEN_HIGH_AND_LOW_VOLTAGE_PROFILE}
+ * to define the voltage ratio relative to nominal voltage, which OpenReac should converge to.
+ * @author Nicolas Pierre <nicolas.pierre at artelys.com>
+ */
+public class OptimisationVoltageRatio implements OpenReacAlgoParam{
+    private static final String RATIO_OBJECTIVE_VOLTAGE_KEY = "ratio_voltage_target";
+    private final double ratio;
+
+    public OptimisationVoltageRatio(double ratio) {
+        this.ratio = ratio;
+    }
+
+    @Override
+    public String getName() {
+        return RATIO_OBJECTIVE_VOLTAGE_KEY;
+    }
+
+    @Override
+    public String getParamValue() {
+        return Double.toString(ratio);
+    }
+}
