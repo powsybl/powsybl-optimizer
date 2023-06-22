33d7d898d02e867e2a58cd00a6fabf6c9090b1bb
Nicolas Pierre <nicolas.pierre@artelys.com>
2023-06-22 10:48:09 +0200

    better customisation for ampl mock
    Signed-off-by: Nicolas Pierre <nicolas.pierre@artelys.com>

--- open-reac/src/test/java/com/powsybl/openreac/OpenReacRunnerTest.java
+++ open-reac/src/test/java/com/powsybl/openreac/OpenReacRunnerTest.java
@@ -45,6 +45,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Nicolas PIERRE <nicolas.pierre at artelys.com>
  */
 class OpenReacRunnerTest {
+    /**
+     * True for CI tests, and people that do not have access to an ampl/knitro licence.
+     * <p>
+     * False if you changed the ampl model and want to test that there is no regression on basic tests.
+     * DO NOT COMMIT your changes to this value.
+     */
+    private static final boolean MOCK_OPEN_REAC = false;
+
+    /**
+     * Folder to run open reac if MOCK_OPEN_REAC is false, relative path are likely start in the open-reac folder
+     * DO NOT COMMIT your changes to this value.
+     */
+    private static final Path OPEN_REAC_TMP_FOLDER = Path.of(System.getProperty("java.io.tmpdir"));
     protected FileSystem fileSystem;
     protected Path tmpDir;
 
@@ -166,7 +179,8 @@ class OpenReacRunnerTest {
         }
     }
 
-    private void testAllModifAndLoadFlow(Network network, String subFolder, OpenReacParameters parameters) throws IOException {
+    private void testAllModifAndLoadFlow(Network network, String subFolder,
+                                         OpenReacParameters parameters) throws IOException {
         LocalCommandExecutor localCommandExecutor = new TestLocalCommandExecutor(
             List.of(subFolder + "/reactiveopf_results_generators.csv",
                 subFolder + "/reactiveopf_results_indic.txt",
@@ -174,10 +188,12 @@ class OpenReacRunnerTest {
                 subFolder + "/reactiveopf_results_shunts.csv",
                 subFolder + "/reactiveopf_results_static_var_compensators.csv",
                 subFolder + "/reactiveopf_results_vsc_converter_stations.csv"));
-        // To really run open reac, use the commentede line below. Be sure that open-reac/src/test/resources/com/powsybl/config/test/config.yml contains your ampl path
-        // try (ComputationManager computationManager = new LocalComputationManager()) {
-        try (ComputationManager computationManager = new LocalComputationManager(new LocalComputationConfig(tmpDir),
-            localCommandExecutor, ForkJoinPool.commonPool())) {
+        // To really run open reac, be sure that open-reac/src/test/resources/com/powsybl/config/test/config.yml contains your ampl path
+        // And do not commit your changes of config.yml !
+        try (ComputationManager computationManager = MOCK_OPEN_REAC ?
+            new LocalComputationManager(new LocalComputationConfig(tmpDir), localCommandExecutor,
+                ForkJoinPool.commonPool())
+            : new LocalComputationManager(new LocalComputationConfig(OPEN_REAC_TMP_FOLDER))) {
             OpenReacResult openReacResult = OpenReacRunner.run(network,
                 network.getVariantManager().getWorkingVariantId(), parameters, new OpenReacConfig(true),
                 computationManager);
@@ -208,7 +224,9 @@ class OpenReacRunnerTest {
     public void testSvc() throws IOException {
         Network network = VoltageControlNetworkFactory.createWithStaticVarCompensator();
         network.getVoltageLevelStream().forEach(vl -> vl.setHighVoltageLimit(380).setHighVoltageLimit(420));
-        network.getStaticVarCompensator("svc1").setVoltageSetpoint(390).setRegulationMode(StaticVarCompensator.RegulationMode.VOLTAGE);
+        network.getStaticVarCompensator("svc1")
+            .setVoltageSetpoint(390)
+            .setRegulationMode(StaticVarCompensator.RegulationMode.VOLTAGE);
         OpenReacParameters parameters = new OpenReacParameters();
         parameters.addConstantQGenerators(List.of("g1"));
         testAllModifAndLoadFlow(network, "openreac-output-svc", parameters);
@@ -243,81 +261,81 @@ class OpenReacRunnerTest {
     public static Network create() {
         Network network = Network.create("svc", "test");
         Substation s1 = network.newSubstation()
-                .setId("S1")
-                .add();
+            .setId("S1")
+            .add();
         Substation s2 = network.newSubstation()
-                .setId("S2")
-                .add();
+            .setId("S2")
+            .add();
         VoltageLevel vl1 = s1.newVoltageLevel()
-                .setId("vl1")
-                .setNominalV(400)
-                .setHighVoltageLimit(420)
-                .setLowVoltageLimit(380)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
+            .setId("vl1")
+            .setNominalV(400)
+            .setHighVoltageLimit(420)
+            .setLowVoltageLimit(380)
+            .setTopologyKind(TopologyKind.BUS_BREAKER)
+            .add();
         vl1.getBusBreakerView().newBus()
-                .setId("b1")
-                .add();
+            .setId("b1")
+            .add();
         vl1.newGenerator()
-                .setId("g1")
-                .setConnectableBus("b1")
-                .setBus("b1")
-                .setTargetP(101.3664)
-                .setTargetV(390)
-                .setMinP(0)
-                .setMaxP(150)
-                .setVoltageRegulatorOn(true)
-                .add();
+            .setId("g1")
+            .setConnectableBus("b1")
+            .setBus("b1")
+            .setTargetP(101.3664)
+            .setTargetV(390)
+            .setMinP(0)
+            .setMaxP(150)
+            .setVoltageRegulatorOn(true)
+            .add();
         VoltageLevel vl2 = s2.newVoltageLevel()
-                .setId("vl2")
-                .setNominalV(400)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
+            .setId("vl2")
+            .setNominalV(400)
+            .setTopologyKind(TopologyKind.BUS_BREAKER)
+            .add();
         vl2.getBusBreakerView().newBus()
-                .setId("b2")
-                .add();
+            .setId("b2")
+            .add();
         vl2.newLoad()
-                .setId("ld1")
-                .setConnectableBus("b2")
-                .setBus("b2")
-                .setP0(101)
-                .setQ0(150)
-                .add();
+            .setId("ld1")
+            .setConnectableBus("b2")
+            .setBus("b2")
+            .setP0(101)
+            .setQ0(150)
+            .add();
         VoltageLevel vl3 = s2.newVoltageLevel()
-                .setId("vl3")
-                .setNominalV(400)
-                .setTopologyKind(TopologyKind.BUS_BREAKER)
-                .add();
+            .setId("vl3")
+            .setNominalV(400)
+            .setTopologyKind(TopologyKind.BUS_BREAKER)
+            .add();
         vl3.getBusBreakerView().newBus()
-                .setId("b3")
-                .add();
+            .setId("b3")
+            .add();
         vl3.newShuntCompensator()
-                .setId("SHUNT")
-                // .setBus("b3")
-                .setConnectableBus("b3")
-                .setSectionCount(0)
-                .setVoltageRegulatorOn(true)
-                .setTargetV(393)
-                .setTargetDeadband(5.0)
-                .newLinearModel()
-                .setMaximumSectionCount(25)
-                .setBPerSection(1e-3)
-                .add()
-                .add();
+            .setId("SHUNT")
+            // .setBus("b3")
+            .setConnectableBus("b3")
+            .setSectionCount(0)
+            .setVoltageRegulatorOn(true)
+            .setTargetV(393)
+            .setTargetDeadband(5.0)
+            .newLinearModel()
+            .setMaximumSectionCount(25)
+            .setBPerSection(1e-3)
+            .add()
+            .add();
         network.newLine()
-                .setId("l1")
-                .setBus1("b1")
-                .setBus2("b2")
-                .setR(1)
-                .setX(3)
-                .add();
+            .setId("l1")
+            .setBus1("b1")
+            .setBus2("b2")
+            .setR(1)
+            .setX(3)
+            .add();
         network.newLine()
-                .setId("l2")
-                .setBus1("b3")
-                .setBus2("b2")
-                .setR(1)
-                .setX(3)
-                .add();
+            .setId("l2")
+            .setBus1("b3")
+            .setBus2("b2")
+            .setR(1)
+            .setX(3)
+            .add();
         return network;
     }
 }
