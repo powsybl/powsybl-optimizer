faebf3bba776c3a862d6983e06dd5b9461770dbb
parvy <pierre.arvy@artelys.com>
2023-11-16 19:27:04 +0100

    Improve unit tests.
    Signed-off-by: parvy <pierre.arvy@artelys.com>

--- open-reac/src/test/java/com/powsybl/openreac/OpenReacParametersTest.java
+++ open-reac/src/test/java/com/powsybl/openreac/OpenReacParametersTest.java
@@ -20,11 +20,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Nicolas PIERRE <nicolas.pierre at artelys.com>
+ * @author Pierre ARVY <pierre.arvy at artelys.com>
  */
 public class OpenReacParametersTest {
 
     @Test
-    public void testObjectiveIntegrityChecks() {
+    public void testObjectiveIntegrity() {
         Network network = IeeeCdfNetworkFactory.create118();
         setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
 
@@ -35,7 +36,7 @@ public class OpenReacParametersTest {
         assertEquals(OpenReacOptimisationObjective.BETWEEN_HIGH_AND_LOW_VOLTAGE_LIMIT, parameters.getObjective());
         parameters.setObjective(OpenReacOptimisationObjective.SPECIFIC_VOLTAGE_PROFILE);
         assertEquals(OpenReacOptimisationObjective.SPECIFIC_VOLTAGE_PROFILE, parameters.getObjective());
-        assertThrows(NullPointerException.class, () -> parameters.setObjective(null), "We can't unset objective function.");
+        assertThrows(NullPointerException.class, () -> parameters.setObjective(null), "Can't unset objective function.");
 
         // Objective distance for BETWEEN_HIGH_AND_LOW_VOLTAGE_LIMIT objective
         assertEquals(0.5, parameters.getObjectiveDistance()); // default value
@@ -43,32 +44,114 @@ public class OpenReacParametersTest {
         assertEquals(0., parameters.getObjectiveDistance());
         parameters.setObjectiveDistance(1.); // max value
         assertEquals(1., parameters.getObjectiveDistance());
-        assertThrows(IllegalArgumentException.class, () -> parameters.setObjectiveDistance(-2.));
-        assertThrows(IllegalArgumentException.class, () -> parameters.setObjectiveDistance(1.2));
+        assertThrows(IllegalArgumentException.class, () -> parameters.setObjectiveDistance(-2.), "Objective distance must be > 0");
+        assertThrows(IllegalArgumentException.class, () -> parameters.setObjectiveDistance(1.2), "Objective distance must be < 1");
         assertThrows(IllegalArgumentException.class, () -> parameters.setObjectiveDistance(Double.NaN), "Objective distance must be defined.");
 
-        assertDoesNotThrow(() -> parameters.checkIntegrity(network));
-        parameters.setObjectiveDistance(0.15);
+        assertTrue(parameters.checkAlgorithmParametersIntegrity());
     }
 
     @Test
-    void testMinMaxVoltageLimitIntegrityChecks() {
+    void testMinMaxVoltageLimitIntegrity() {
+        Network network = IeeeCdfNetworkFactory.create118();
+        setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
+
         OpenReacParameters parameters = new OpenReacParameters();
 
         // Consistency of min plausible low voltage limit (>= 0)
-        assertThrows(InvalidParametersException.class, () -> parameters.setMinPlausibleLowVoltageLimit(-0.25));
+        assertEquals(0.5, parameters.getMinPlausibleLowVoltageLimit()); // default value
+        parameters.setMinPlausibleLowVoltageLimit(0.);
+        assertEquals(0., parameters.getMinPlausibleLowVoltageLimit()); // min value
         parameters.setMinPlausibleLowVoltageLimit(0.8211);
         assertEquals(0.8211, parameters.getMinPlausibleLowVoltageLimit());
+        assertThrows(InvalidParametersException.class, () -> parameters.setMinPlausibleLowVoltageLimit(-0.25), "minPlausibleLowVoltageLimit must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setMinPlausibleLowVoltageLimit(Double.NaN), "minPlausibleLowVoltageLimit must be defined.");
 
         // Consistency of max plausible high voltage limit (> 0)
-        assertThrows(InvalidParametersException.class, () -> parameters.setMaxPlausibleHighVoltageLimit(-0.15));
-        assertThrows(InvalidParametersException.class, () -> parameters.setMaxPlausibleHighVoltageLimit(0));
+        assertEquals(1.5, parameters.getMaxPlausibleHighVoltageLimit()); // default value
         parameters.setMaxPlausibleHighVoltageLimit(0.75);
         assertEquals(0.75, parameters.getMaxPlausibleHighVoltageLimit());
+        assertThrows(InvalidParametersException.class, () -> parameters.setMaxPlausibleHighVoltageLimit(-0.15), "maxPlausibleHighVoltageLimit must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setMaxPlausibleHighVoltageLimit(0), "maxPlausibleHighVoltageLimit must be > 0");
+        assertThrows(InvalidParametersException.class, () -> parameters.setMaxPlausibleHighVoltageLimit(Double.NaN), "maxPlausibleHighVoltageLimit must be defined.");
 
         // Check min < max
         assertFalse(parameters.checkAlgorithmParametersIntegrity());
         parameters.setMaxPlausibleHighVoltageLimit(1.2);
+        assertTrue(parameters.checkAlgorithmParametersIntegrity());
+    }
+
+    @Test
+    void testAlphaCoefficientIntegrity() {
+        Network network = IeeeCdfNetworkFactory.create118();
+        setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
+
+        OpenReacParameters parameters = new OpenReacParameters();
+        assertEquals(1., parameters.getAlphaCoefficient()); // default/max value
+        parameters.setAlphaCoefficient(0.); // min value
+        assertEquals(0., parameters.getAlphaCoefficient());
+        parameters.setAlphaCoefficient(0.445556);
+        assertEquals(0.445556, parameters.getAlphaCoefficient());
+
+        assertThrows(InvalidParametersException.class, () -> parameters.setAlphaCoefficient(-1.2), "alphaCoefficient must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setAlphaCoefficient(42.), "alphaCoefficient must be < 1.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setAlphaCoefficient(Double.NaN), "alphaCoefficient must be defined.");
+        assertTrue(parameters.checkAlgorithmParametersIntegrity());
+    }
+
+    @Test
+    void testZeroPowerThresholdIntegrity() {
+        Network network = IeeeCdfNetworkFactory.create118();
+        setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
+
+        OpenReacParameters parameters = new OpenReacParameters();
+        assertEquals(0.01, parameters.getZeroPowerThreshold()); // default value
+        parameters.setZeroPowerThreshold(0.);
+        assertEquals(0., parameters.getZeroPowerThreshold()); // min value
+        parameters.setZeroPowerThreshold(2.365);
+        assertEquals(2.365, parameters.getZeroPowerThreshold());
+
+        assertThrows(InvalidParametersException.class, () -> parameters.setZeroPowerThreshold(-1.2), "zeroPowerThreshold must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setZeroPowerThreshold(Double.NaN), "zeroPowerThreshold must be defined.");
+        assertTrue(parameters.checkAlgorithmParametersIntegrity());
+    }
+
+    @Test
+    void testZeroImpedanceThresholdIntegrity() {
+        Network network = IeeeCdfNetworkFactory.create118();
+        setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
+
+        OpenReacParameters parameters = new OpenReacParameters();
+        assertEquals(1e-4, parameters.getZeroImpedanceThreshold()); // default value
+        parameters.setZeroImpedanceThreshold(0.);
+        assertEquals(0., parameters.getZeroImpedanceThreshold()); // min value
+        parameters.setZeroImpedanceThreshold(1e-5);
+        assertEquals(1e-5, parameters.getZeroImpedanceThreshold());
+
+        assertThrows(InvalidParametersException.class, () -> parameters.setZeroImpedanceThreshold(-1.2), "zeroImpedanceThreshold must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setZeroImpedanceThreshold(Double.NaN), "zeroImpedanceThreshold must be defined.");
+    }
+
+    @Test
+    void testNominalThresholdsIntegrity() {
+        Network network = IeeeCdfNetworkFactory.create118();
+        setDefaultVoltageLimits(network); // set default voltage limits to every voltage levels of the network
+
+        OpenReacParameters parameters = new OpenReacParameters();
+        assertEquals(1., parameters.getNominalThresholdIgnoredBuses()); // default value
+        parameters.setNominalThresholdIgnoredBuses(0.); // min value
+        assertEquals(0., parameters.getNominalThresholdIgnoredBuses());
+        parameters.setNominalThresholdIgnoredBuses(45.);
+        assertEquals(45., parameters.getNominalThresholdIgnoredBuses());
+        assertThrows(InvalidParametersException.class, () -> parameters.setNominalThresholdIgnoredBuses(-1.2), "nominalThresholdIgnoredBuses must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setNominalThresholdIgnoredBuses(Double.NaN), "nominalThresholdIgnoredBuses must be defined.");
+
+        assertEquals(0., parameters.getNominalThresholdIgnoredVoltageBounds()); // default/min value
+        parameters.setNominalThresholdIgnoredVoltageBounds(200.);
+        assertEquals(200., parameters.getNominalThresholdIgnoredVoltageBounds());
+        assertThrows(InvalidParametersException.class, () -> parameters.setNominalThresholdIgnoredVoltageBounds(-1.2), "nominalThresholdIgnoredVoltageBounds must be > 0.");
+        assertThrows(InvalidParametersException.class, () -> parameters.setNominalThresholdIgnoredVoltageBounds(Double.NaN), "nominalThresholdIgnoredVoltageBounds must be defined.");
+
         assertTrue(parameters.checkAlgorithmParametersIntegrity());
     }
 
